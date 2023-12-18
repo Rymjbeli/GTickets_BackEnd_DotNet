f7af6576b6a22ae5606a3d85d9ab23ec7f030dba
= <mohamedaziz.benghorbel@insat.ucar.tn>
2023-12-18 18:07:08 +0100

    IRepository added

---
+++ GTickets_BackEnd/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace GTickets_BackEnd.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
